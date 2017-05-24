b6e1cedced9bf337bdd9a489b07fa761a2b8efe3
johnnydoubt <cesar.tailleur@gmail.com>
2017-05-24 16:40:37 +0200

    End

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,13 +1,39 @@
-$('#maincontainer').scroll(function() {
-    //On stocke la valeur du scroll
-    var top = $(window).scrollTop();
-    console.log(top);
-    //Si la scroll est supérieur à 250px
-    if (top > 460) {
-        //On ajouter la classe sticky au maincontainer
-        $('.fadeInBlock').animate({
-            'opacity': '1'
-        }, 100);
-
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html,body').css('overflow','visible');
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
     }
-});
+    $('html').css('overflow','hidden');
+    $('body').css('overflow','');
+  });
